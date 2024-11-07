e70646feeeb1add3b96c27c947828a91b97b509f
RyotaOku <ryota1991o@gmail.com>
2024-11-07 10:12:43 +0900

    fix: 本番環境に向けてディレクトリ変更

--- src/app/createAccount/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function CreateAccount() {
-    return (
-        <div>
-            <h1 className='mt-20 ml-6 leading-10 text-3xl font-bold'>ようこそ、<br />@ryota11_07</h1>
-        </div>
-    )
-} 
--- src/app/userSetup/page.tsx
+++ src/app/userSetup/page.tsx
@@ -1,7 +1,72 @@
 'use client'
 import Button from '@/app/component/common/Button'
+import Chevron from '@/app/component/common/Chevron'
+import Checkbox from '@/app/component/common/Checkbox'
+import { useState } from 'react'
 
-export default function accountSetting() {
+
+export default function CreateAccount() {
+    return (
+        <div>
+            <h1 className='mt-20 ml-6 leading-10 text-3xl font-bold'>ようこそ、<br />@ryota11_07</h1>
+        </div>
+    )
+}
+
+export function CreateAccountTel() {
+    const [number, setNumber] = useState<string>('森尾')
+
+    return (
+        <div className='mt-20 text-xl text-center font-bold'>
+            <h1 className='text-[32px] leading-10'>電話番号を<br />入力してください</h1>
+            <p className='border-b-4 w-fit m-auto mt-[60px] mb-[40px] text-center'>
+                <input type='tel' value={number} onChange={(e) => {
+                    setNumber(e.target.value.replace(/[^\d]/g, ''));
+                }} />
+            </p>
+            <p>
+                {/* <button className='w-[294px] h-[39px] text-white rounded-xl bg-[#3570c6]'>認証コードを受け取る</button> */}
+                <Button disabled={false} text={'認証コードを受け取る'} onClick={() => {
+                    console.log('void');
+                }} />
+            </p>
+        </div>
+    )
+}
+
+
+export function Regulations() {
+    return (
+        <div className='flex flex-col gap-10 justify-center h-screen mx-10'>
+            <h1 className='mx-auto text-3xl'>利用するために</h1>
+            <div>
+                <Checkbox text={'エンドユーザーライセンス契約'} />
+                <p className='pl-5 text-xs text-gray-400'>これには、アプリの安全性、セキュリティ機能を保証するために、「アプリ名」が「アプリ名」本体含む各機能のソフトウェアを自動的に随時更新することに同意することも含まれます。</p>
+                <p className='w-fit ml-6 text-xs border-b-2 '>詳細</p>
+            </div>
+            <div>
+                <Checkbox text={'利用規約'} />
+                <p className='pl-6 text-xs text-gray-400'>これには、アプリの安全性、セキュリティ機能を保証するために、「アプリ名」が「アプリ名」本体含む各機能のソフトウェアを自動的に随時更新することに同意することも含まれます。</p>
+                <p className='w-fit ml-6 text-xs border-b-2 '>詳細</p>
+            </div>
+            <div>
+                <Checkbox text={'プライバシーポリシー'} />
+                <p className='w-fit ml-6 text-xs border-b-2 '>詳細</p>
+            </div>
+            <div className='mx-auto border-dashed border-t-2 w-full'></div>
+            <div>
+                <input type='checkbox' className='mr-2' />
+                <label htmlFor=''>全てに同意<span className='text-gray-400'>(任意)</span></label>
+            </div>
+
+            <a href='/accountSetting' className='w-fit mt-20 py-2 px-4 self-end rounded-3xl text-white bg-[#3570c6] '>
+                <Button disabled={false} text={'同意する'} onClick={() => { }} />
+            </a>
+        </div>
+    )
+}
+
+export function accountSetting() {
     return (
         <div className='font-bold'>
             <div className='ml-10 mt-44'>
@@ -13,6 +78,104 @@ export default function accountSetting() {
                     <Button disabled={false} text={'次へ'} onClick={() => { }} />
                 </a>
             </div>
+        </div>
+    )
+}
+
+export function gender() {
+    return (
+        <div className='font-bold'>
+            <p className='mt-44 text-center text-3xl'>あなたの性別を<br />教えてください</p>
+            <div className='flex justify-center gap-4 mt-8'>
+                <p className='flex justify-center items-end w-36 h-36 pb-4 rounded-full text-center text-white bg-[#3570c6]'>男性</p>
+                <p className='flex justify-center items-end w-36 h-36 pb-4 rounded-full text-center bg-gray-200'>女性</p>
+            </div>
+            <Chevron prevLink='/accountSetting' nextLink='/accountSetting/age' />
+        </div>
+    )
+}
+
+export function age() {
+    return (
+        <div className='font-bold'>
+            <div className='flex justify-center gap-4 mt-24'>
+                <p className='flex justify-center items-end w-32 h-32 pb-4 rounded-full text-center text-white bg-[#3570c6]'>男性</p>
+                <p className='flex justify-center items-end w-32 h-32 pb-4 rounded-full text-center bg-gray-200'>女性</p>
+            </div>
+            <p className='mt-16 text-center text-3xl'>あなたの年代を<br />教えてください</p>
+            <div className='mt-12 text-center'>
+                <select className='border-b-2 border-gray-400 text-center font-normal'>
+                    <option>10代</option>
+                    <option>20代</option>
+                    <option>30代</option>
+                    <option>40代</option>
+                    <option>50代</option>
+                    <option>60代以上</option>
+                </select>
+            </div>
+            <Chevron prevLink='/accountSetting/gender' nextLink='/accountSetting/name' />
+        </div>
+    )
+}
+
+export function name() {
+    return (
+        <div className='font-bold'>
+            <p className='mt-44 text-center text-3xl'>表示名を設定<br />しましょう</p>
+            <div className='mt-12 px-12 text-center'>
+                <input type='text' className='w-full pl-2 border-b-2 border-gray-400' />
+            </div>
+            <Chevron prevLink='/accountSetting/age' nextLink='/accountSetting/permit' />
+        </div>
+    )
+}
+
+import { FaLocationArrow } from 'react-icons/fa';
+import { FaBell } from 'react-icons/fa';
+import { FaBluetooth } from 'react-icons/fa';
+
+export function permit() {
+    return (
+        <div className='font-bold'>
+            <p className='mt-36 text-center text-2xl'>最後に以下を許可してください</p>
+            <div className='flex flex-col gap-8 mt-12 m-4 p-4 rounded bg-gray-100'>
+                <div className='flex items-center gap-4 px-4'>
+                    <FaLocationArrow size={50} />
+                    <div>
+                        <p>位置情報の利用</p>
+                        <p className='font-normal'>すれちがい機能のために必要です。権限がなくても利用できますが、誰ともすれちがえなくなります。</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-4 px-4'>
+                    <FaBell size={50} />
+                    <div>
+                        <p>通知の設定</p>
+                        <p className='font-normal'>すれちがいできた時、誰かがあなたにコンタクトを求めた時、その他様々なシーンでお知らせします</p>
+                    </div>
+                </div>
+                <div className='flex items-center gap-4 px-4'>
+                    <FaBluetooth size={50} />
+                    <div>
+                        <p>Bluetoothの有効化</p>
+                        <p className='font-normal'>GPSのみの位置情報は性格ではありません。Bluetoothの有効化によって消費する電力はごくわずかです。</p>
+                    </div>
+                </div>
+            </div>
+            <div className='w-fit mt-20 mx-auto px-36 py-2 rounded-3xl text-center text-white bg-[#3570c6] '>
+                <a href='/accountSetting/fin'><Button disabled={false} text={'すすむ'} onClick={() => { }} /></a>
+            </div>
+        </div>
+    )
+}
+
+export function accountSetting2() {
+
+    return (
+        <div className='h-screen flex flex-col justify-center gap-28 text-center text-3xl font-bold'>
+            <h1>おつかれさまでした</h1>
+            <h2>すれちがいを<br />始めましょう！</h2>
+            <img src='../images/woman.png' alt='' className='pr-4 w-[250px] self-end' />
+
         </div>
     )
 }
